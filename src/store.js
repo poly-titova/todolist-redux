bd338731e1c5f13fc85d143a0daa13a6a54bdafb
Poly Titova <poly_titova@mail.ru>
2021-01-23 16:28:56 +0300

    Добавили redux

--- src/store.js
+++ src/store.js
@@ -3,18 +3,18 @@ import rootReducer from './reducers/index';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+  process.env.NODE_ENV !== 'production' &&
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 /* eslint-enable */
 
 const configureStore = preloadedState => (
-  createStore(
-    rootReducer,
-    preloadedState,
-    composeEnhancers(),
-  )
+  createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(),
+  )
 );
 
 const store = configureStore({});
